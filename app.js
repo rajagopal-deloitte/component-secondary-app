5732d4a9847d485b0ac9885b64115d7f71cc34ec
rkurupati <rajagopal_kurupati@cat.com>
2019-05-22 13:34:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use((req, res, next) => {
+    console.log(req.url);
+    console.log(req.body);
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/', (req, res) => {
+    res.sendFile('/index.html');
+});
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
